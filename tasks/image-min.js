f43f6b655f19f33ece6459994121f32fe5a57c4b
alexdcrv <dikarevaleksandr0@gmail.com>
2022-05-24 23:23:59 +0300

    unmin

--- tasks/image-min.js
+++ tasks/image-min.js
@@ -5,26 +5,26 @@
 
 const gulp = require('gulp');
 const newer = require('gulp-newer');
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 
 module.exports = function (options) {
 
   return () => {
     return gulp.src(`./${options.src}/images/**/*`)
       .pipe(newer(`./${options.dest}/images/`))
-      .pipe(imagemin([
-        imagemin.jpegtran({
-          progressive: true
-        }),
-        imagemin.optipng({
-          optimizationLevel: 5
-        }),
-        imagemin.svgo({
-          plugins: [{
-            removeViewBox: false
-          }]
-        })
-      ]))
+      // .pipe(imagemin([
+      //   imagemin.jpegtran({
+      //     progressive: true
+      //   }),
+      //   imagemin.optipng({
+      //     optimizationLevel: 5
+      //   }),
+      //   imagemin.svgo({
+      //     plugins: [{
+      //       removeViewBox: false
+      //     }]
+      //   })
+      // ]))
       .pipe(gulp.dest(`./${options.dest}/images/`));
   };
 };
--- tasks/image-webp.js
+++ tasks/image-webp.js
@@ -5,7 +5,7 @@
 
 const gulp = require('gulp');
 const newer = require('gulp-newer');
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const imageminWebp = require('imagemin-webp');
 const rename = require('gulp-rename');
 //const changed = require('gulp-changed');
@@ -17,7 +17,7 @@ module.exports = function (options) {
         .src(`./${options.src}/images/**/*.{jpg,png}`)
         .pipe(newer({ dest: `./${options.dest}/images/`, ext: '.webp' }))
         //.pipe(changed(`./${options.dest}/images/`, { extension: '.webp' }))
-        .pipe(imagemin([imageminWebp({ quality: 80 })]))
+        // .pipe(imagemin([imageminWebp({ quality: 80 })]))
         .pipe(
           rename({
             extname: '.webp',

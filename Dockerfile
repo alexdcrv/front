6349548d73725c021b991177eddaf8602db93ec7
alexdcrv <dikarevaleksandr0@gmail.com>
2022-05-30 20:35:29 +0300

    connect wallet

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM node:14
 WORKDIR /app
 
 COPY package*.json ./
-RUN apt-get update && apt-get install -y libglu1 && apt-get install -y libjpeg-dev
-RUN yarn install
 
+RUN apt-get update && apt-get install -y libglu1 && apt-get install -y libjpeg-dev
+
+RUN yarn install
 
 COPY . .
 
--- src/js/app.js
+++ src/js/app.js
@@ -26,6 +26,160 @@ import tabs from "./components/tabs";
 
 		////
 
+    const Web3Modal = window.Web3Modal.default;
+    const WalletConnectProvider = window.WalletConnectProvider.default;
+    const Fortmatic = window.Fortmatic;
+    const evmChains = window.evmChains;
+    
+    let web3Modal
+    
+    
+    let provider;
+    
+    let selectedAccount;
+    
+    function init() {
+    
+      console.log("Initializing example");
+      console.log("WalletConnectProvider is", WalletConnectProvider);
+      console.log("Fortmatic is", Fortmatic);
+      console.log("window.web3 is", window.web3, "window.ethereum is", window.ethereum);
+    
+    //   if(location.protocol !== 'https:') {
+    //     // https://ethereum.stackexchange.com/a/62217/620
+    //     const alert = document.querySelector("#alert-error-https");
+    //     alert.style.display = "block";
+    //     document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
+    //     return;
+    //   }
+    
+      
+      const providerOptions = {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
+          }
+        },
+    
+        fortmatic: {
+          package: Fortmatic,
+          options: {
+            key: "pk_test_391E26A3B43A3350"
+          }
+        }
+      };
+    
+      web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions, 
+        disableInjectedProvider: false, 
+      });
+    
+      console.log("Web3Modal instance is", web3Modal);
+    }
+    
+    
+    
+    async function fetchAccountData() {
+      const web3 = new Web3(provider);
+    
+      console.log("Web3 instance is", web3);
+    
+      const chainId = await web3.eth.getChainId();
+      const chainData = evmChains.getChain(chainId);
+      // document.querySelector("#network-name").textContent = chainData.name;
+    
+      const accounts = await web3.eth.getAccounts();
+    
+      console.log("Got accounts", accounts);
+      selectedAccount = accounts[0];
+    
+      // document.querySelector("#selected-account").textContent = selectedAccount;
+      // const template = document.querySelector("#template-balance");
+      // const accountContainer = document.querySelector("#accounts");
+      // accountContainer.innerHTML = '';
+    
+      const rowResolvers = accounts.map(async (address) => {
+        // const balance = await web3.eth.getBalance(address);
+        // const ethBalance = web3.utils.fromWei(balance, "ether");
+        // const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
+        // const clone = template.content.cloneNode(true);
+        // clone.querySelector(".address").textContent = address;
+        // clone.querySelector(".balance").textContent = humanFriendlyBalance;
+        // accountContainer.appendChild(clone);
+      });
+    
+      await Promise.all(rowResolvers);
+      document.querySelector("#prepare").style.display = "none";
+      document.querySelector("#prepare-mob").style.display = "none";
+      document.querySelector("#connected").style.display = "block";
+      document.querySelector("#connected-mob").style.display = "block";
+    }
+    
+    async function refreshAccountData() {
+      document.querySelector("#connected").style.display = "none";
+      document.querySelector("#connected-mob").style.display = "none";
+      document.querySelector("#prepare").style.display = "block";
+      document.querySelector("#prepare-mob").style.display = "block";
+      document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
+      document.querySelector("#btn-connect-mob").setAttribute("disabled", "disabled")
+      await fetchAccountData(provider);
+      document.querySelector("#btn-connect").removeAttribute("disabled")
+      document.querySelector("#btn-connect-mob").removeAttribute("disabled")
+    }
+    
+    async function onConnect() {
+    
+      console.log("Opening a dialog", web3Modal);
+      try {
+        provider = await web3Modal.connect();
+      } catch(e) {
+        console.log("Could not get a wallet connection", e);
+        return;
+      }
+    
+      provider.on("accountsChanged", (accounts) => {
+        fetchAccountData();
+      });
+    
+      provider.on("chainChanged", (chainId) => {
+        fetchAccountData();
+      });
+    
+      provider.on("networkChanged", (networkId) => {
+        fetchAccountData();
+      });
+    
+      await refreshAccountData();
+    }
+    
+    async function onDisconnect() {
+    
+      console.log("Killing the wallet connection", provider);
+    
+      if(provider.close) {
+        await provider.close();
+        await web3Modal.clearCachedProvider();
+        provider = null;
+      }
+    
+      selectedAccount = null;
+      document.querySelector("#prepare").style.display = "block";
+      document.querySelector("#prepare-mob").style.display = "block";
+      document.querySelector("#connected").style.display = "none";
+      document.querySelector("#connected-mob").style.display = "none";
+    }
+    
+    
+    window.addEventListener('load', async () => {
+      init();
+      document.querySelector("#btn-connect").addEventListener("click", onConnect);
+      document.querySelector("#btn-connect-mob").addEventListener("click", onConnect);
+      document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
+      document.querySelector("#btn-disconnect-mob").addEventListener("click", onDisconnect);
+    });
+
     const HEADER = document.querySelector('.js-fixed-header');
     const CLASS_FIXED = 'fixed';
 
@@ -101,3 +255,4 @@ import tabs from "./components/tabs";
 
   });
 })(jQuery);
+
